e58562f9416f8fa4114bb8d52e4de59debf5ed80
Sumit19Chakraborty <tbiggbull@gmqil.com>
2024-09-06 08:22:00 +0530

    update

--- components/work.tsx
+++ components/work.tsx
@@ -8,7 +8,7 @@ const Work = () => {
       <div className='container w-full py-[100px] lg:py-[200px]'>
         <h1 className='mt-8 px-2 py-4 text-[40px] font-bold uppercase leading-[3rem] md:px-0'>
           <br />
-          <span className='under-line'>Course's</span>
+          <span className='under-line'>Courses</span>
         </h1>
         <div className='flex flex-col gap-[200px] py-4 xl:gap-[150px]'>
           {projectsData.map((project, i) => (
